6502eb0cefe4296e25b327e3d119b8d143d4714f
Весняна <vesnyanavlasova@yandex.ru>
2025-10-30 09:43:34 +0400

    Update lesson_3_task_1.py

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,6 +2,6 @@ from user import User
 
 my_user = User("Иван", "Иванов")
 
-print(my_user. get_first_name())
+print(my_user.get_first_name())
 print(my_user.get_last_name())
 print(my_user.get_name_info())
